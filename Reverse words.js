70ac443131d036a03bf3244eb8a6ff60a2e3b166
irfan-s-m <know.cse@gmail.com>
2024-03-14 01:04:57 +0600

    reverse words prblm ongoing

---
+++ Reverse words.js
@@ -0,0 +1,17 @@
+function reverseWords(str) {
+    const splitStr = str.split("  ");
+    let reverseStr = '';
+    for (const arrayItem of splitStr) {
+        for (const item of arrayItem) {
+            reverseStr = item + reverseStr;
+        }
+    }
+    const reverseArr = reverseStr.split(" ");
+    const arr = reverseArr.reverse();
+    const joinStr = arr.join(" ");
+    return joinStr;
+  }
+
+
+
+  console.log(reverseWords("This is an example!"))
