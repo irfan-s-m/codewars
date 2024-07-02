7bc390fe633256cc6f419bd0f734ce26a08d460b
irfan-s-m <know.cse@gmail.com>
2024-07-03 00:23:15 +0600

    Total amounts of points problem solved

--- Total amount of points.js
+++ Total amount of points.js
@@ -1,3 +1,18 @@
 function points(games) {
-    return 0
-  }
+  let totalPoints = 0;
+  games.forEach(i => {
+    if (parseInt(i[0]) > parseInt(i[2])) {
+      totalPoints = totalPoints + 3;
+    }
+    else if (parseInt(i[0]) < parseInt(i[2])) {
+      totalPoints = totalPoints + 0;
+    }
+    else if (parseInt(i[0]) == parseInt(i[2])) {
+      totalPoints = totalPoints + 1;
+    }
+  })
+  return totalPoints;
+}
+
+games = ['1:2', '2:2', '3:2', '4:3', '1:3']
+points(games)
