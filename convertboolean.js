a41b9de51f60695bde3b006dd0b5c03ddec62df2
irfan-s-m <know.cse@gmail.com>
2024-07-06 00:16:10 +0600

    Convert Boolean to string problem solved

---
+++ convertboolean.js
@@ -0,0 +1,10 @@
+function boolToWord(bool) {
+    return bool ? 'Yes' : 'No';
+}
+
+const input = true;
+
+console.log(boolToWord(input))
+
+
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
